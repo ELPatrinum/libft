48a411abe4de3474607fca4b334a550795bd42c2
elpatrinum <meb-bak@outlook.fr>
2023-11-07 16:10:21 +0100

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,21 @@
 /*   By: muel-bak <muel-bak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:50:57 by muel-bak          #+#    #+#             */
-/*   Updated: 2023/11/07 14:37:36 by muel-bak         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:04:56 by muel-bak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+long	calc_result(long tmp, long result, char c, int sign)
+{
+	tmp = (result * 10) + (c - 48);
+	if (tmp < result && sign == 1)
+		return (-1);
+	else if (tmp < result && sign == -1)
+		return (0);
+	return (tmp);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -22,24 +32,19 @@ int	ft_atoi(const char *str)
 	i = 0;
 	sign = 1;
 	result = 0;
+	tmp = 0;
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	if (str[i] == '-')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		sign = -1;
+		if (str[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	else if (str[i] == '+')
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result = calc_result(tmp, result, str[i], sign);
 		i++;
-	while (str[i] >= '0' && str[i] <= '9')
-    {
-        tmp = (result * 10) + (str[i] - 48);
-        if (tmp < result && sign  == 1)
-            return (-1);
-        else if (tmp < result && sign  == -1)
-            return (0);
-        result = tmp;
-        i++;
-    }
+	}
 	return (sign * result);
 }
